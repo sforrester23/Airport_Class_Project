38dc43c15b32ebe0b6c9273a40c976494c1b567b
Sam Forrester <SForrester@spartaglobal.com>
2019-09-27 10:29:34 +0100

    Added a class for Customers that is a subclass of People, with attributes

--- Customer_Class.py
+++ Customer_Class.py
@@ -1,1 +1,13 @@
+from People_Class import *
+
+# Define new subclass of people
 class Customer(People):
+    def __init__(self, first_name, last_name, age, email, phone_number, passport_number, flight_number, destination_airport, origin_airport):
+        # Inherit the same attributes as People class
+        super().__init__(first_name, last_name, age, email, phone_number)
+        self.passport_number = passport_number
+        self.flight_number = flight_number
+        self.destination_airport = destination_airport
+        self.origin_airport = origin_airport
+
+
